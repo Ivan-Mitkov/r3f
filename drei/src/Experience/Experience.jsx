e48e3c27366c803a7580d10f93b8a4667b9c9e00
Ivan Mitkov <ivan.mitkov@gmail.com>
2023-02-19 10:45:44 +0200

    add orbit controls from drei

--- drei/src/Experience/Experience.jsx
+++ drei/src/Experience/Experience.jsx
@@ -1,30 +1,13 @@
-import { extend, useFrame, useThree } from "@react-three/fiber";
-import { useRef, useEffect } from "react";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-// to extend three  we need to use extend, choose the class and the name
-extend({ OrbitControls: OrbitControls });
+import { useRef } from "react";
+import { OrbitControls } from "@react-three/drei";
 
 function Experience() {
-  // get the same state as in useFrame but only when component mount, not on each frame
-  const { camera, gl } = useThree();
-
   const cubeRef = useRef();
   const group = useRef();
 
-  // the scene is already being drawn on each frame but we can’t see it because nothing is moving.
-  // We are going to rotate the cube, and to do that, we are going to use the useFrame hook provided by R3F
-  useFrame((state, delta) => {});
-
-  useEffect(() => {
-    const controls = new OrbitControls(camera, gl.domElement);
-
-    return () => {
-      controls.dispose();
-    };
-  }, [camera, gl]);
-
   return (
     <>
+      <OrbitControls />
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
